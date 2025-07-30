748af67ba959d8b0d6dc255672a5ab21575b1613
K00lka <69790586+K00lka@users.noreply.github.com>
2025-07-30 22:17:29 +0200

    Game without a counter

---
+++ main.js
@@ -0,0 +1,43 @@
+'use strict';
+function randomNumber() {
+    const targetNumber = Math.floor(Math.random() * 100) + 1;
+    return targetNumber
+} 
+
+function userGuess() {
+    let guess = parseFloat(prompt("Enter a number between 1 and 100:"));
+    if (isNaN(guess) || guess < 1 || guess > 100 || !Number.isInteger(guess)) {
+        alert("Please enter a valid number between 1 and 100.");
+        return userGuess();
+    }
+    return guess;
+}
+
+function checkGuess (guess, targetNymber){
+    if (guess < targetNymber) {
+        alert("Too low! Try again.");
+        return false;
+    } else if (guess > targetNymber) {
+        alert("Too high! Try again.");
+        return false;
+    } else {
+        alert("Congratulations! You've guessed the number!");
+        return true;
+    }
+}
+
+function game() {
+    let targetNumber =randomNumber();
+    let guess = userGuess();
+    let result = checkGuess(guess, targetNumber);
+    const maxAttempts = 10;
+    let attempts = 1;
+    while (!result && attempts < maxAttempts) {
+        attempts++;
+        const guess = userGuess();
+        result = checkGuess(guess, targetNumber);
+    }
+   
+}
+
+game()
